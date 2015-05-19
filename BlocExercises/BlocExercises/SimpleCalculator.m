eaaaac8a355e98b14e8d4ad01b93b1450f77e125
Kerolos <keronakhla@gmail.com>
2015-05-18 18:54:25 -0700

    Simple Calculator Checkpoint

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -11,26 +11,23 @@
 @implementation SimpleCalculator
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
-    NSInteger newInteger = 6;
-    NSInteger number = newInteger + 1;
-    return number;
+    return number++;
 }
-                                //why is number1 is in the method below
+
+
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
-    NSInteger number1 = 1;      //I didn't even declare it anywhere else
-    NSInteger number2 = 6;      //I didn't even declare it anywhere else
-    
+   
     NSInteger sumNumber = number1 + number2;
     
     return sumNumber;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
-    NSInteger six = 6;
-    NSInteger four = 4;
     
-    NSInteger newNumber = six % four;
-    return 0;           //why not returning newNumber?
+    NSInteger number = dividend % divisor;
+    return number;
+    
+    return 0;
 }
 
 @end
