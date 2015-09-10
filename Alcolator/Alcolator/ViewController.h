c3de5dc23196a96e6a1de2b811c0c05c91c6ca63
Kerolos <keronakhla@gmail.com>
2015-09-09 22:41:33 -0700

    checkpoing 21,
    ls
    cd ..

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Kerolos Nakhla on 9/3/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
+@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+- (void)buttonPressed:(UIButton *)sender; 
+
+@end
+
